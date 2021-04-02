e9cca080b432f7741ef910a282385efabec0058b
unknown <claraqn@naver.com>
2021-04-02 18:11:26 +0900

    0402

--- userdao2/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ userdao2/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,11 +1,52 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@Configuration
+//Configuration = spring이 관리하는 영역이라고 선언하는 것
 public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
+
+    //annotation으로 값을 주입하는 것
+    //이걸 사용하려면 edit configuration에다가 값을 추가해줘야 사용가능
+    @Value("${db.driver}")
+    private String className;
+    @Value("${db.username}")
+    private String root;
+    @Value("${db.password}")
+    private String rootpw;
+    @Value("${db.url}")
+    private String url;
+
+    @Bean
+    public UserDao userDao() throws ClassNotFoundException {
+
+        return new UserDao(dataSource());
     }
 
-    public JejuConnectionMaker getConnectionMaker() {
-        return new JejuConnectionMaker();
+    @Bean
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource =
+                new SimpleDriverDataSource();
+        // "com.mysql..." -> mysql 써야만 동작
+        // -> runtimeonly(실행시 로딩이 필요한 라이브러리라는뜻)이기 때문에 클래스를 로드하는 부분이 필요
+        // 그래야만 오라클,mysql 골라서 사용이 가능
+        className = "com.mysql.cj.jdbc.Driver";
+        root = "root";
+        rootpw = "rootpw";
+        url = "jdbc:mysql://localhost/user_db?" + "characterEncoding=utf-8&serverTimezone=UTC";
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        dataSource.setUsername(root);
+        dataSource.setPassword(rootpw);
+        dataSource.setUrl(url);
+
+        return dataSource;
     }
+
+
 }
--- userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -1,22 +1,37 @@
 package kr.ac.jejunu;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+    static UserDao userDao; // **static 지정 = 영역 메모리 사용
+
+    @BeforeAll
+    public static void setup(){
+        //ApplicationContext = bean을 관리하는 기본 오브젝트, bean을 다루는 툴
+        ApplicationContext applicationContext
+                // spring에서 관리하는 DaoFactory의 클래스를 사용
+                = new AnnotationConfigApplicationContext(DaoFactory.class);
+        //userDao 라는 이름으로 UserDao라는 클래스에 bean을 줘~ 라는 뜻
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Integer id = 1;
         String name = "clara";
         String password = "1234";
 
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+//        DaoFactory daoFactory = new DaoFactory();
+//        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -32,16 +47,57 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
+//        DaoFactory daoFactory = new DaoFactory();
+//        UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
 
         User insertedUser = userDao.findById(user.getId()); // insert된 user 나올것
         assertThat(user.getId(), greaterThan(0));
         assertThat(insertedUser.getId(), is(user.getId())); // id값은 0보다 클것이라고 예상하는것
-        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getName(), is(user.getName())); //assertThat함수->값이 같으면 성공 다르면 실패 반환
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
+
+    @Test
+    public void update() throws SQLException {
+        String name = "clara";
+        String password = "1234";
+
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        user.setName("xiajunsu");
+        user.setPassword("1111");
+
+        userDao.update(user);
+
+        User updatedUser = userDao.findById(user.getId());
+        assertThat(updatedUser.getId(), is(user.getId()));
+        assertThat(updatedUser.getName(), is(user.getName()));
+        assertThat(updatedUser.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        String name = "xiajunsu";
+        String password = "1111";
+
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.findById(user.getId());
+        assertThat(deletedUser, nullValue());
+
+    }
+
 //
 //    @Test
 //    public void getHalla() throws SQLException, ClassNotFoundException {
